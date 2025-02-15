c5b68d972e7061f340f3efd5e2e90201bad60aab
Wal1ac6 <1679924552@qq.com>
2025-02-15 16:23:29 +0800

    HelloWorld

---
+++ day01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // 主方法，程序的入口
+    public static void main(String[] args) {
+        // 输出字符串"Hello, World!"到控制台
+        System.out.println("Hello, World!");
+    }
+}
